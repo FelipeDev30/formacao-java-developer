8e763e657f860c9795494a991c1b77d23ee6a536
= <felipedevjob29@hotmail.com>
2026-06-05 16:51:19 -0300

    Aprendendo a sintaxe JAVA

---
+++ java-basico/src/Myclass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Myclass {
+    
+    public static void main (String [] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scan.nextLine();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scan.nextLine();
+
+        String nomeCompleto = nomeCompleto(nome, sobrenome);
+        System.out.println("O nome completo é: " + nomeCompleto);
+    }
+
+    public static String nomeCompleto (String nome, String sobrenome) {
+        return nome.concat(" ").concat(sobrenome);
+    }
+}
