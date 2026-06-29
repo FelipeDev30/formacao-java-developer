cc527e33e6224e4316fca1b11dc0b5a1b651415d
= <=>
2026-06-28 23:05:48 -0300

    Revisando estudos e corrigindo erros

--- java-basico/src/edu/felipe/variaveis/TiposVariaveis.java
+++ java-basico/src/edu/felipe/variaveis/TiposVariaveis.java
@@ -3,7 +3,7 @@ package edu.felipe.variaveis;
 public class TiposVariaveis {
      public static void main(String [] args){
         byte idade = 123;
-        short ano = 2021;
+        short ano = 32;
         int cep = 21770080;
         long cpf = 17172315782L;
         float preco = 329.99f;
@@ -16,13 +16,12 @@ public class TiposVariaveis {
         System.out.println(preco);
         System.out.println(PI);
 
-        String meuNome = "FELIPE";
-        ano = 2022;
-        System.out.println("Meu nome é " + meuNome);
+        String meuNome = "Felipe Lamas";
+        ano = 32;
         System.out.println("Meu nome é " + meuNome + "e tenho " + ano + " anos");
 
-        ano = 2023;
-        System.out.println("Meu nome é " + meuNome);
+        meuNome = "Rodrigo Lamas";
+        ano = 28;
         System.out.println("Meu nome é " + meuNome + "e tenho " + ano + " anos");
      }
 }
